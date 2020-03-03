78c7c2a065b55826f10c66a3f157519d677156d8
mmrsic <mmrsic@web.de>
2020-03-03 23:40:07 +0100

    Extended tests  * Added test cases for examples of User's Reference Guide

--- src/test/kotlin/com/github/mmrsic/ti99/usersrefguide/PrintStatementTest.kt
+++ src/test/kotlin/com/github/mmrsic/ti99/usersrefguide/PrintStatementTest.kt
@@ -332,7 +332,7 @@ class PrintStatementTest {
     }
 
     @Test
-    fun testColonSeparators() {
+    fun testTwoColonSeparators() {
         val machine = TiBasicModule()
         val interpreter = TiBasicCommandLineInterpreter(machine)
         interpreter.interpret("PRINT \"A\"::\"B\"", machine)
@@ -343,6 +343,72 @@ class PrintStatementTest {
                 19 to " >PRINT \"A\"::\"B\"",
                 20 to "  A",
                 22 to "  B",
+                24 to " >"
+            ), machine.screen
+        )
+    }
+
+    @Test
+    fun testSemicolonSeparators() {
+        val machine = TiBasicModule()
+        val interpreter = TiBasicCommandLineInterpreter(machine)
+        interpreter.interpretAll(
+            """
+            100 A=-26
+            110 B=-33
+            120 C$="HELLO"
+            130 D$="HOW ARE YOU?"
+            140 PRINT A;B;C$;D$
+            150 END
+            RUN
+            """.trimIndent(), machine
+        )
+
+        TestHelperScreen.assertPrintContents(
+            mapOf(
+                11 to "  TI BASIC READY",
+                13 to " >100 A=-26",
+                14 to " >110 B=-33",
+                15 to " >120 C$=\"HELLO\"",
+                16 to " >130 D$=\"HOW ARE YOU?\"",
+                17 to " >140 PRINT A;B;C$;D$",
+                18 to " >150 END",
+                19 to " >RUN",
+                20 to "  -26 -33 HELLOHOW ARE YOU?",
+                22 to "  ** DONE **",
+                24 to " >"
+            ), machine.screen
+        )
+    }
+
+    @Test
+    fun testColonSeparators() {
+        val machine = TiBasicModule()
+        val interpreter = TiBasicCommandLineInterpreter(machine)
+        interpreter.interpretAll(
+            """
+            100 A=-26
+            110 B$="HELLO"
+            120 C$="HOW ARE YOU?"
+            130 PRINT A:B$:C$
+            140 END
+            RUN
+            """.trimIndent(), machine
+        )
+
+        TestHelperScreen.assertPrintContents(
+            mapOf(
+                10 to "  TI BASIC READY",
+                12 to " >100 A=-26",
+                13 to " >110 B$=\"HELLO\"",
+                14 to " >120 C$=\"HOW ARE YOU?\"",
+                15 to " >130 PRINT A:B$:C$",
+                16 to " >140 END",
+                17 to " >RUN",
+                18 to "  -26",
+                19 to "  HELLO",
+                20 to "  HOW ARE YOU?",
+                22 to "  ** DONE **",
                 24 to " >"
             ), machine.screen
         )
