25cd301ee640d4d23b5f54c2313572ecddc234de
mmrsic <mmrsic@web.de>
2020-08-13 20:07:34 +0200

    Fixed missing method implementation

--- src/main/kotlin/com/github/mmrsic/ti99/hw/Colors.kt
+++ src/main/kotlin/com/github/mmrsic/ti99/hw/Colors.kt
@@ -14,9 +14,8 @@ class TiBasicColors {
    /** [TiCharacterColor] for a given [TiColorSet]. */
    fun getColor(charSet: TiColorSet) = colorSets.getOrDefault(charSet, defaultCharColor)
 
-   fun forCharacter(charCode: Int) {
-
-   }
+   /** [TiColorSet] for a given TI Basic character code. */
+   fun forCharacter(charCode: Int) = TiColorSet.forCharacterCode(charCode)
 
    /** Reset the colors of this instance to its default. */
    fun reset() {
